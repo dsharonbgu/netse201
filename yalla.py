ee508753f0ede34d3a5a6c50f7623bf95c04a12b
dsharonbgu <dsharon@post.bgu.ac.il>
2020-01-23 17:49:57 +0200

    first empty

---
+++ yalla.py
@@ -0,0 +1,28 @@
+import os
+from zipfile import ZipFile
+
+
+
+def main():
+    #print('hello for hidden file into an image constructor\n in order to start please provide an occuret absolute path of the files')
+    hidden_file_abs_path= input("Enter path of the file you want to hide:\n")
+    hidden_file_name = os.path.basename(hidden_file_abs_path)
+    hidden_file_dir_path = os.path.dirname(hidden_file_abs_path)
+
+    image_file_abs_path= input("Enter path of the image should be cover:\n")
+    img_file_name = os.path.basename(image_file_abs_path)
+    img_file_dir_path = os.path.dirname(image_file_abs_path)
+
+
+
+    os.chdir(hidden_file_dir_path)
+    ZipFile('hd.zip','w').write(hidden_file_name)
+
+    hidden_zipfile_abs_path= hidden_file_dir_path +'\hd.zip'
+    os.chdir(img_file_dir_path)
+    command = 'copy /b '+img_file_name + ' + '+hidden_zipfile_abs_path
+    os.system(command)
+
+
+if __name__ == "__main__":
+    main()
